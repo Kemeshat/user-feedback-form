0b5d243b83671525f7b0137ca068ac704f79108c
Kemeshat <kemeshat@usf.edu>
2026-03-15 14:27:21 -0400

    implemented tooltip display on mouseover and hide on mouseout

--- cp_1.js
+++ cp_1.js
@@ -5,4 +5,28 @@ comments.addEventListener('input', function (){
 charCount.textContent = comments.value.length;
 });
 
+const form = document.getElementById('feedback-form');
+form.addEventListener ("mouseover", function (event){
+    if (event.target.tagName === "INPUT"|| event.target.tagName === "TEXTAREA"){
+
+const tip= event.target.nextElementSibling;
+if (tip){
+    tip.style.display = "block";
+}
+    }
+});
+
+form.addEventListener ("mouseout", function (event){
+    if (event.target.tagName === "INPUT"|| event.target.tagName === "TEXTAREA"){
+const tip = event.target.nextElementSibling;
+
+if (tip){
+tip.style.display = "none";
+}
+
+    }
+});
+
+
+
 
